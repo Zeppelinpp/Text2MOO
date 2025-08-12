d65c890ba60e0292dfa446fa11090261c4b3a2b4
Zeppelinpp <1056118649@qq.com>
2025-08-12 13:39:32 +0800

    Add base types model

---
+++ src/text2moo/models/types.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel, Field
+from typing import List, Optional, Any, Dict
+
+
+class BaseUnit(BaseModel):
+    """
+    Base unit of user's data.
+    """
+
+    id: str
+    name: Optional[str] = None
+    attributes: Dict[str, Any] = Field(
+        default_factory=dict, description="Attributes of the unit."
+    )
+
+
+class OptimizationGroup(BaseModel):
+    """
+    Search space of optimization based on user's data.
+    """
+
+    unit_attr: List[str] = Field(
+        default_factory=list, description="Attribute keys of the unit."
+    )
+    units: List[BaseUnit] = Field(
+        default_factory=list, description="Units of the search space."
+    )
